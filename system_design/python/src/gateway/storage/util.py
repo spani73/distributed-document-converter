d2543fd2faaa6f690819a06ecccd0fa44d25c70a
spani73 <subhankar.panigra45@gmail.com>
2025-03-03 21:57:59 +0530

    adding rabbit mq changes

--- system_design/python/src/gateway/storage/util.py
+++ system_design/python/src/gateway/storage/util.py
@@ -13,4 +13,8 @@ def upload(f, fs, channel, access):
     }
     
     try:
-        channel.basic_publish(exchange="", routing_key="upload", body=json.dumps(message))
+        channel.basic_publish(exchange="", routing_key="upload", body=json.dumps(message),
+            properties= pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE)) # We need to make sure the message is not lost if kubernetes pod is restarted.
+    except Exception as err:
+        fs.delete(fid)
+        return "internal server error", 500
